bd58d4d24f6572b5aa601ebc12604b78b5446975
Boobalan Kothandaraman <kboobalan93@gmail.com>
2021-11-18 22:35:38 +0530

    Add project files.

---
+++ LogProject/Services/LogService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection;
+using System.Xml;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+using Xamarin.Essentials;
+
+namespace LogProject
+{
+    /// <summary>
+    /// Log all stacktraces depending on their level : Info, Error, Fatal, Info, Warning.
+    /// </summary>
+    public class LogService : ILogService
+    {
+        private Logger logger;
+
+        public void Initialize(Assembly assembly, string assemblyName)
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("logfile") 
+            { 
+                FileName = $"{FileSystem.AppDataDirectory}/{DateTime.Now.Date.ToString("yyyy-M-dd")}.txt",
+                DeleteOldFileOnStartup=true,
+                EnableFileDelete=true,
+                FileNameKind=FilePathKind.Relative,
+                Name="file",
+            };
+
+            // Rules for mapping loggers to targets            
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+            // Apply config           
+            NLog.LogManager.Configuration = config;
+
+            //var location = $"{assemblyName}.NLog.config";
+            //var stream = assembly.GetManifestResourceStream(location);
+            //LogManager.Configuration = new XmlLoggingConfiguration(XmlReader.Create(stream), null);
+            
+            this.logger = LogManager.GetCurrentClassLogger();
+        }
+
+        public void LogDebug(string message)
+        {
+            this.logger.Info(message);
+        }
+
+        public void LogError(string message)
+        {
+            this.logger.Error(message);
+        }
+
+        public void LogFatal(string message)
+        {
+            this.logger.Fatal(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            this.logger.Info(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            this.logger.Warn(message);
+        }
+    }
+}
---
+++ LogProject/ViewModels/ViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+using Xamarin.Essentials;
+
+namespace LogProject
+{
+    public class ViewModel : ViewModelBase
+    {
+        private readonly string eventTrackerMessage = "User click to generate crash!";
+        private readonly ILogService logService;
+        private readonly ICloudLogService cloudLogService;
+
+        public ICommand GenerateLog { get; set; }
+
+        public ViewModel(ILogService logService, ICloudLogService cloudLogService)
+        {
+            this.logService = logService;
+            this.cloudLogService = cloudLogService;
+            this.GenerateLog = new RelayCommand(GenerateException);
+        }
+
+        /// <summary>
+        /// Demo exceptions.
+        /// </summary>
+        private async void GenerateException()
+        {
+            try
+            {
+                this.cloudLogService.LogEvent(eventTrackerMessage);
+                // Simulate a crash
+                throw new Exception();
+            }
+            catch (Exception ex)
+            {
+                this.logService.LogInfo("This is not an exception for...");
+                this.logService.LogError(ex.ToString());
+                this.cloudLogService.LogError(ex);
+
+                System.Diagnostics.Debug.WriteLine($"App folder path :{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}");
+                
+                var file = Directory.GetFiles(FileSystem.AppDataDirectory, "*.txt").LastOrDefault();                if (!string.IsNullOrEmpty(file))                {                    var logList = new ObservableCollection<string>(File.ReadAllLines(file));                }
+            }
+        }
+    }
+}
